5dd57609ebdae17fcd73658876dbe918ef0ea36d
Gustavo Melo <gustavo.melo@vagas.com.br>
2016-09-06 19:44:29 -0300

    organiza os links do menu lateral

--- app/router.js
+++ app/router.js
@@ -12,13 +12,16 @@ class Router {
 		app.post('/login', (req, res) => controllers.loginController.login(req, res));
 		app.post('/logout', (req, res) => controllers.loginController.logout(req, res));
 
-		app.get('/teatros/novo', helpers.auth.authorize, (req, res) => controllers.theaterController.new(req, res));
-		app.post('/teatros/novo', helpers.auth.authorize, (req, res) => controllers.theaterController.create(req, res));
+		app.get('/teatros/*', helpers.auth.authorize);
+		app.get('/teatros/*', helpers.theatersByUser.getTheaters.bind(helpers.theatersByUser));
 
-		app.get('/teatros/:theaterId(\\d+)/eventos', helpers.auth.authorize, helpers.theatersByUser.getTheaters.bind(helpers.theatersByUser), (req, res) => controllers.eventsController.index(req, res));
+		app.get('/teatros/novo', (req, res) => controllers.theaterController.new(req, res));
+		app.post('/teatros/novo', (req, res) => controllers.theaterController.create(req, res));
 
-		app.get('/teatros/:theaterId(\\d+)/eventos/novo', helpers.auth.authorize, helpers.theatersByUser.getTheaters, (req, res) => controllers.eventsController.new(req, res));
-		app.post('/teatros/:theaterId(\\d+)/eventos/novo', helpers.auth.authorize, helpers.theatersByUser.getTheaters, (req, res) => controllers.eventsController.create(req, res));
+		app.get('/teatros/:theaterId(\\d+)/eventos', (req, res) => controllers.eventsController.index(req, res));
+
+		app.get('/teatros/:theaterId(\\d+)/eventos/novo', (req, res) => controllers.eventsController.new(req, res));
+		app.post('/teatros/:theaterId(\\d+)/eventos/novo', (req, res) => controllers.eventsController.create(req, res));
 	}
 }
 
