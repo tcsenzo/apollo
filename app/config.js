f101736da6f86448bc37963ae6f59c674675ee47
Gustavo Melo <gustavo.melo@vagas.com.br>
2016-09-22 21:37:40 -0300

    troca o dateFormat pelo moment.js, ajusta as datas para o novo formato

--- app/config.js
+++ app/config.js
@@ -1,7 +1,7 @@
 let config = {
-  theaterEventsApi: "http://events.qettal.com",
-  checkoutApi: "http://checkout.qettal.com",
-  authApi: "http://auth.qettal.com",
+  theaterEventsApi: "http://localhost:8080",
+  checkoutApi: "http://localhost:8082",
+  authApi: "http://localhost:8083",
 }
 
 module.exports = config;
